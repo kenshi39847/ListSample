5a9175bf630a11c64900916f7a72945ebc501fd9
kenshi39847 <kenshi.t.blue@gmail.com>
2024-03-02 19:01:46 +0900

    レイヤ、コンポーネント

--- src/chap09/Chao09Study.java
+++ src/chap09/Chao09Study.java
@@ -24,5 +24,27 @@ public class Chao09Study {
 |6 |クイズ画面を表示する|GET         |/quiz/play  |
 |7 |クイズを解く        |POST        |/quiz/check |
 
+アプリケーションを作成する際はレイヤで分けることが推奨されている。
+今回は以下の3レイヤで分割する。
+○アプリケーション層
+クライアントから受け取ったリクエストを制御し、ドメイン層を使ってアプリケーションを制御する
+Controller、Form、View
+○ドメイン層
+Domain Objectに対するアプリケーションのサービス処理を実行する
+Domain Object、Service、Repository
+○インフラストラクチャ層
+Domain Objectに対するCRUD操作を行い、データ永続化を担う
+Repositorympl、O/R Mapper
 
+○コンポーネント一覧
+|No|層                    |コンポーネント|名称          |備考                     |
+|1 |アプリケーション層    |Controller    |QuizController|制御の役割               |
+|2 |アプリケーション層    |Form          |QuizForm      |画面のフォームを表現     |
+|3 |アプリケーション層    |View          |-             |見た目                   |
+|4 |ドメイン層            |Domain Object |Quiz          |Entityと同様             |
+|5 |ドメイン層            |Service       |QuizService   |インターフェースで作成   |
+|6 |ドメイン層            |Servicempl    |QuizServicempl|Serviceを実装            |
+|7 |ドメイン層            |Repository    |QuizRepository|インターフェースで作成   |
+|8 |インフラストラクチャ層|Repositorympl |-             |O/Rマッパーにより自動作成|
+|9 |インフラストラクチャ層|O/R Mapper    |-             |Spring Data JDBCを使用   |
 */
